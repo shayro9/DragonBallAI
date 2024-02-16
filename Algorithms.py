876b856f69c14140945a9bc7a6c474ad6b274cfd
shayro9 <shayro13@gmail.com>
2024-02-16 16:13:55 +0200

    fixed expanded

--- Algorithms.py
+++ Algorithms.py
@@ -12,7 +12,7 @@ class BFSAgent():
     def search(self, env: DragonBallEnv) -> Tuple[List[int], float, int]:
         agent = env.get_initial_state()
         if env.is_final_state(agent):
-            return [], 0,0
+            return [], 0, 0
         open_nodes = [agent]
         closed_nodes = []
         price_dict = {agent: [0, []]}
@@ -23,19 +23,20 @@ class BFSAgent():
                 env.reset()
                 env.set_state(agent)
                 price = child[1]
+                terminated = child[2]
                 if price == float('inf'):
                     continue
                 new_step = env.step(action)
                 new_child = new_step[0]
                 new_value = price + price_dict[agent][0]
                 if not new_child in closed_nodes and not new_child in open_nodes:
-                    actions_untill_now = price_dict[agent][1].copy()
-                    actions_untill_now.append(action)
-                    price_dict[new_child] = [new_value, actions_untill_now]
-                    print(f"for the node: {new_child} we have the price: {new_value}\n")
-                    if env.is_final_state(new_child):
-                        return price_dict[new_child][1], new_value, len(closed_nodes)
-                    open_nodes.insert(0, new_child)
+                    if not terminated or env.is_final_state(new_child):
+                        actions_untill_now = price_dict[agent][1].copy()
+                        actions_untill_now.append(action)
+                        price_dict[new_child] = [new_value, actions_untill_now]
+                        if env.is_final_state(new_child):
+                            return price_dict[new_child][1], new_value, len(closed_nodes)
+                        open_nodes.insert(0, new_child)
 
 
 class WeightedAStarAgent():
